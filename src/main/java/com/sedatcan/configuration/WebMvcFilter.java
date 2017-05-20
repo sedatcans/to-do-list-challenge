689f2fbd1aaf6ccf52e951657dab37dba6084c4b
Sedatcan Sonat <sedatcan@gmail.com>
2017-05-20 17:08:44 +0300

    fixing filters for swagger

--- src/main/java/com/sedatcan/configuration/WebMvcFilter.java
+++ src/main/java/com/sedatcan/configuration/WebMvcFilter.java
@@ -3,6 +3,7 @@ package com.sedatcan.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -12,5 +13,17 @@ public class WebMvcFilter extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
     }
 }
